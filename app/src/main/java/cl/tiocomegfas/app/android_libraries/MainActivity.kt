83920909467b6e97cf924a95269f92cba82336cd
Fredy Moncada <fredymoncada777@gmail.com>
2023-10-23 09:15:34 -0300

    Add: application base

---
+++ app/src/main/java/cl/tiocomegfas/app/android_libraries/MainActivity.kt
@@ -0,0 +1,11 @@
+package cl.tiocomegfas.app.android_libraries
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
