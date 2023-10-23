221bbbcc5c43027b654522b4974c34fa6c330c12
Fredy Moncada <fredymoncada777@gmail.com>
2023-10-23 09:16:09 -0300

    Add: gradle files

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Android-Libraries"
+include(":app")
+include(":bluetooth")
+include(":ui-view")
+include(":network")
